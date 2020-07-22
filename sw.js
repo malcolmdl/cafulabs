806013bbe826a0e156ca9d5a8950847608a0c16f
malcolmdl <61853748+malcolmdl@users.noreply.github.com>
2020-07-21 20:58:44 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,57 @@
+const staticCacheName = "site-static";
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/style.css',
+    '/css/materialize.min.css',
+    '/css/materialize.css',
+    '/hexa1.jpg', 
+    '/background1.jpg',
+    '/background2.jpg',
+    '/background3.jpg',
+    '/js/init.js',
+    '/js/materialize.js',
+    '/manifest.webmanifest',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+
+];
+
+self.addEventListener("install", evt=> {
+    //console.log("service worker has been installed");
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log("caching shell assets");
+            cache.addAll(assets); 
+        })
+    );
+
+});
+//service worker
+self.addEventListener("activate",evt=> {
+    //console.log("service worker has been activated");
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+                
+            )
+        })
+    );
+});
+//fetch event
+self.addEventListener("fetch", evt => {
+    //console.log("fetch event", evt);
+    //evt.respondWith(
+       // caches.match(evt.request).then(cacheRes=> {
+           // return cacheRes || fetch(evt.request);
+
+        //})
+    //);
+});
